c3ce06f4eb32ec8af490c73d00399bf29bf1c4a5
LaughingManDotEXE <35981510+LaughingManDotEXE@users.noreply.github.com>
2018-05-31 00:57:18 -0500

    UpdateMagicSkill in Project
    Finished up the update for the skills.

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 
 from sqlalchemy import create_engine, asc
@@ -24,6 +24,12 @@ mages = session.query(Mage).order_by(asc(Mage.name))
 #Show a Magic_Skill
 @app.route('')
 def showMagicSkill():
+@app.route('/mage/<int:mage_id>/')
+@app.route('/mage/<int:mage_id>/menu/')
+def showMagicSkills(mage_id):
+    mage = session.query(Mage).filter_by(id = mage_id).one()
+    items = session.query(MagicSkill).filter_by(mage_id = mage_id).all()
+    return render_template('menu.html', items = items, mage = mage)
 #Delete a Mage
 @app.route('/mage/<int:mage_id>/delete/', methods = ['GET','POST'])
 def deleteMage(mage_id):
@@ -68,12 +74,26 @@ def updateMage():
 	else:
 		return
 #Update a Magic_Skill
-@app.route('', methods = ['GET','POST'])
-def updateMagicSkill():
-	if
-		return
-	else:
-		return
+@app.route('/mage/<int:mage_id>/menu/<int:skill_id>/edit', methods=['GET','POST'])
+def editMagicSkill(mage_id, skill_id):
+
+    editedSkill = session.query(MagicSkill).filter_by(id = skill_id).one()
+    mage = session.query(Mage).filter_by(id = mage_id).one()
+    if request.method == 'POST':
+        if request.form['skillName']:
+            editedSkill.skillName = request.form['skillName']
+        if request.form['description']:
+            editedSkill.description = request.form['description']
+        if request.form['power']:
+            editedSkill.power = request.form['power']
+        if request.form['course']:
+            editedSkill.course = request.form['course']
+        session.add(editedSkill)
+        session.commit() 
+        flash('Magic skill Successfully Edited')
+        return redirect(url_for('showMagicSkills', mage_id = mage_id))
+    else:
+        return render_template('edit_magic_skill.html', mage_id = mage_id, skill_id = skill_id, item = editedSkill)
 #JSON API for all Mage information
 @app.route('')
 def
